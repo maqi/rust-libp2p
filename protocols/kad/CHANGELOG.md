9200d45475f1f299feba84f61e48bab9695f7bfb
maqi <qi.ma@maidsafe.net>
2025-05-06 22:24:31 +0800

    feat(kad): configurable outbound_substreams timeout
    Then testing `put_record_to` with poor connectioin but large packet size (around 4MB), the `put_record_to` frequently got failed and the PutRecordToError::stats showing always failed at 10s.
    ```
    "kad_event::PutRecordError::QuorumFailed", required quorum 2, stored on [], QueryStats { requests: 5, success: 0, failure: 5, start: Some(Instant { tv_sec: 121323, tv_nsec: 961187829 }), end: Some(Instant { tv_sec: 121334, tv_nsec: 188694869 }) } - ProgressStep { count: 1, last: true }
    ```
    However, this timeout is not defined by request/response protocol timeout (which in our case increased to 30s already).
    
    Further digging through the libp2p code, it turned out this timeout is actually affected by outbound_substreams timeout.
    We tested with increase to 30s, do see much less put_record_to failure and QueryStats showing extended completion duration (larger than 10s, and in case of failed, to be 30s).
    
    Hence raised this PR to provide a configurable approach of the timeout.
    
    Pull-Request: #6015.

--- protocols/kad/CHANGELOG.md
+++ protocols/kad/CHANGELOG.md
@@ -14,6 +14,8 @@
   See [PR 5715](https://github.com/libp2p/rust-libp2p/pull/5715).
 - Remove deprecated default constructor for `ProtocolConfig`.
   See [PR 5774](https://github.com/libp2p/rust-libp2p/pull/5774).
+- Configurable outbound_substreams_timeout.
+  See [PR 6015](https://github.com/libp2p/rust-libp2p/pull/6015).
 
 <!-- Update to libp2p-core v0.43.0 -->
 
--- protocols/kad/src/handler.rs
+++ protocols/kad/src/handler.rs
@@ -24,7 +24,6 @@ use std::{
     marker::PhantomData,
     pin::Pin,
     task::{Context, Poll, Waker},
-    time::Duration,
 };
 
 use either::Either;
@@ -453,6 +452,8 @@ impl Handler {
             }
         }
 
+        let outbound_substreams_timeout = protocol_config.outbound_substreams_timeout_s();
+
         Handler {
             protocol_config,
             mode,
@@ -461,7 +462,7 @@ impl Handler {
             next_connec_unique_id: UniqueConnecId(0),
             inbound_substreams: Default::default(),
             outbound_substreams: futures_bounded::FuturesTupleSet::new(
-                Duration::from_secs(10),
+                outbound_substreams_timeout,
                 MAX_NUM_STREAMS,
             ),
             pending_streams: Default::default(),
